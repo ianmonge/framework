9cf2a8430283568489c8679243b3cebc75805a4d
Ian Monge <ian.monge@socialpoint.es>
2013-03-20 15:54:20 +0100

    Created file 'init.php' and adapted the other files

--- bye.php
+++ bye.php
@@ -1,11 +1,6 @@
 <?php
- 
-require_once __DIR__.'/autoload.php';
- 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
- 
-$request = Request::createFromGlobals();
- 
-$response = new Response('Goodbye!');
+
+require_once __DIR__.'/init.php';
+
+$response->setContent('Goodbye!');
 $response->send();
